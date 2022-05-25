8053a3057940f9b5681c9088e13dafdcca78dcb5
tsk.haraguchi@gmail.com <tsk.haraguchi@gmail.com>
2022-05-25 18:44:07 +0900

    [add] bin dir

--- randomArr.py
+++ randomArr.py
@@ -5,8 +5,8 @@ def run(n):
 	l = list(range(n))
 	random.shuffle(l)
 	str_l = " ".join([str(n) for n in l])
-	command1 = f"./push_swap {str_l} | wc -l | tr -d ' '"
-	command2 = f"./push_swap {str_l} | ./checker_Mac {str_l}"
+	command1 = f"./bin/push_swap {str_l} | wc -l | tr -d ' '"
+	command2 = f"./bin/push_swap {str_l} | ./bin/checker_Mac {str_l}"
 	count = subprocess.run(command1, shell=True, capture_output=True, text=True)
 	ok_ko = subprocess.run(command2, shell=True, capture_output=True, text=True)
 	return int(count.stdout), ok_ko.stdout
